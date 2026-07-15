738cf744315f82ff3e043309ef8117d711994ea4
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-07-15 14:00:40 +0500

    CRUD

--- app9/views.py
+++ app9/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView, ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
 from .models import Phones
 from .serializers import PhonesSerializer
-# Create your views here.
-class PhonesViewSet(viewsets.ModelViewSet):
+
+
+class PhonesListAPIView(ListAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesRetrieveAPIView(RetrieveAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesCreateAPIView(CreateAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesUpdateAPIView(UpdateAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesDestroyAPIView(DestroyAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesListCreateAPIView(ListCreateAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesRetrieveDestroyAPIView(RetrieveDestroyAPIView):
+    queryset = Phones.objects.all()
+    serializer_class = PhonesSerializer
+
+
+class PhonesRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Phones.objects.all()
     serializer_class = PhonesSerializer
